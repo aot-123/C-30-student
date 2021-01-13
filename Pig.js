ddd0a3b35300d06e57f89ad657663f64b1e9d671
Attack on Titan <ansu.yovan@gmail.com>
2021-01-13 17:53:34 +0530

    angrybirdsstage4.5

--- Pig.js
+++ Pig.js
@@ -12,7 +12,12 @@ class Pig extends BaseClass {
     }
     else{
       World.remove(world, this.body);
-    }
+      push();
+      this.Visibility = this.Visibility-5;
+      tint(255, this.Visibility);
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+      pop();
+      }
   }
 
 };
